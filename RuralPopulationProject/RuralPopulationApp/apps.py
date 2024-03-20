9f6147f9873f13b8fda0562818e384a76787a8f3
QIANCHENG LUO <q.luo.23@abdn.ac.uk>
2024-03-20 19:23:43 +0000

    delete RuarlPopilationProject

--- RuralPopulationProject/RuralPopulationApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RuralpopulationappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'RuralPopulationApp'
--- RuralPopulationProject/RuralPopulationProject/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def homepage(request):
-    return render(request, 'home.html')
-
-def about(request):
-    return render(request, 'about.html')
