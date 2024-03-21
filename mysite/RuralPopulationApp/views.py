4426794180d882ca714dbf63718690364b6e35b5
WEI ZHANG <w.zhang2.23@abdn.ac.uk>
2024-03-21 12:19:30 +0000

    change the views

--- mysite/RuralPopulationApp/views.py
+++ mysite/RuralPopulationApp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from .models import Country,DataEntry,Region,IncomeGroup,RegionCountries,IncomeCountries
 
 # Create your views here.
-
 def countries(request):
-    return render(request, 'RuralPopulationApp/countries.html')
-
+    countries=Country.objects.all()
+    regions=Region.objects.all()
+    income_groups=IncomeGroup.objects.all()
+    # Merge the three dictionaries into one
+    context = {
+        'countries': countries,
+        'regions': regions,
+        "income_groups": income_groups
+    }
+    return render(request, 'RuralPopulationApp/countries.html', context)
+    
 def income_groups(request):
     return render(request, 'RuralPopulationApp/income_groups.html')
 
